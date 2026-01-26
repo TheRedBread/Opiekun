c3354ccef01f21cdb6d575a4014cad37a2790552
RedBread <trefel.yt@gmail.com>
2026-01-26 12:29:03 +0100

    Update ZasobDTO.cs

--- Opiekun/Models/Dtos/ZasobDTO.cs
+++ Opiekun/Models/Dtos/ZasobDTO.cs
@@ -3,11 +3,11 @@
 public class ZasobDTO
 {
     public Guid Id { get; set; }
-    public string Nazwa { get; set; } = String.Empty;
-    public string Kategoria {  get; set; } = String.Empty;
+    public string Nazwa { get; set; } = string.Empty;
+    public string Kategoria {  get; set; } = string.Empty;
     public decimal Ilosc { get; set; }
-    public string Jednostka { get; set; } = String.Empty;
-    public string MiejscePrzechowania { get; set; } = String.Empty;
+    public string Jednostka { get; set; } = string.Empty;
+    public string MiejscePrzechowania { get; set; } = string.Empty;
 
     public bool CzyNiskiStan { get; set; }
     public decimal MinimumIlosc { get; set; }
